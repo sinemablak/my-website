61f9c3f7cf92bc428f6a9666c7135bb2d8c94582
sinemablak <sinemablakk1@gmail.com>
2024-07-18 10:14:05 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from "./components/Header/Header";
 
 import { useData } from "./contexts/ContextData";
 import { Spinner } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const { data } = useData();
